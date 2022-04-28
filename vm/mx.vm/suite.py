db6e3519a4ccf4a0af9cd2526e505e452416f0a2
Michal Karm Babacek <karm@redhat.com>
2022-04-28 16:52:03 +0200

    Unmark suites and bump version to 22.1.0.1 [skip ci]

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -1,8 +1,8 @@
 suite = {
     "name": "vm",
-    "version" : "22.1.0.0",
+    "version" : "22.1.0.1",
     "mxversion" : "5.317.12",
-    "release" : True,
+    "release" : False,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
