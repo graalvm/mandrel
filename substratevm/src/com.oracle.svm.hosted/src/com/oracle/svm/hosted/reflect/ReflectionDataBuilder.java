oracle/graal

Throw exception for null in RuntimeJNIAccess/RuntimeReflection reg.
Don't allow null values to be passed to the `register` method of
`RuntimeJNIAccess` and `RuntimeReflection`. Since these are public APIs
GraalVM should either handle null values (by ignoring them in this case)
or throw a `NullPointerException` before creating an asynchronous task
to perform the registration in the analysis, which then results in
`NullPointerException`s being thrown later when it's no longer possible
to understand where the null value originate from.

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/reflect/ReflectionDataBuilder.java
@@ -272,6 +272,10 @@ public class ReflectionDataBuilder extends ConditionalConfigurationRegistry impl
         checkNotSealed();
         register(analysisUniverse -> registerConditionalConfiguration(condition, () -> {
             for (Executable executable : executables) {
+                if (executable == null) {
+                    throw new NullPointerException("Cannot register null value as executable for reflection. " +
+                                    "Please ensure that all values you register are not null.");
+                }
                 analysisUniverse.getBigbang().postTask(debug -> registerMethod(queriedOnly, executable));
             }
         }));
@@ -404,6 +408,10 @@ public class ReflectionDataBuilder extends ConditionalConfigurationRegistry impl
     private void registerInternal(ConfigurationCondition condition, Field... fields) {
         register(analysisUniverse -> registerConditionalConfiguration(condition, () -> {
             for (Field field : fields) {
+                if (field == null) {
+                    throw new NullPointerException("Cannot register null value as field for reflection. " +
+                                    "Please ensure that all values you register are not null.");
+                }
                 analysisUniverse.getBigbang().postTask(debug -> registerField(field));
             }
         }));

graalvm/mandrel

(cherry picked from commit e6c12dd389609f0eac07aa93d085fa33dd21bce0)

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/reflect/ReflectionDataBuilder.java
@@ -154,6 +154,10 @@ public class ReflectionDataBuilder extends ConditionalConfigurationRegistry impl
 
     private void registerMethods(boolean queriedOnly, Executable[] methods) {
         for (Executable method : methods) {
+            if (method == null) {
+                throw new NullPointerException("Cannot register null value as method for reflection. " +
+                        "Please ensure that all values you register are not null.");
+            }
             ExecutableAccessibility oldValue;
             ExecutableAccessibility newValue;
             do {
@@ -178,6 +182,10 @@ public class ReflectionDataBuilder extends ConditionalConfigurationRegistry impl
     private void registerFields(Field[] fields) {
         // Unsafe and write accesses are always enabled for fields because accessors use Unsafe.
         for (Field field : fields) {
+            if (field == null) {
+                throw new NullPointerException("Cannot register null value as field for reflection. " +
+                        "Please ensure that all values you register are not null.");
+            }
             if (reflectionFields.add(field)) {
                 modifiedClasses.add(field.getDeclaringClass());
             }