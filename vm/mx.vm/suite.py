d259f14366180a240ae0130d83f349bf2d211d76
ol-automation_ww <ol-automation_ww@oracle.com>
2026-08-28 21:08:05 +0000

    new dev cycle 25.0.4.1.1
    (cherry picked from commit c7dd46d2d55ea5bca43c21ef9ca52b967c3f6b04)

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -1,8 +1,8 @@
 suite = {
     "name": "vm",
-    "version" : "25.0.4.1",
+    "version" : "25.0.4.1.1",
     "mxversion": "7.34.1",
-    "release" : True,
+    "release" : False,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "eba5be12437ddf85b56b6d50180ce93e6769f72b",
+                "version": "531a1b90c3191d77d48a675676db713dbfff78f2",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,14 +42,14 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "eba5be12437ddf85b56b6d50180ce93e6769f72b",
+                "version": "531a1b90c3191d77d48a675676db713dbfff78f2",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
             },
             {
                 "name": "truffleruby",
-                "version": "e05618e0c96774bfadc6475adbe9bd513b171285",
+                "version": "d629de5908e130047375e5c1a5a37801757701c9",
                 "dynamic": True,
                 "urls": [
                     {"url": "https://github.com/oracle/truffleruby.git", "kind": "git"},
@@ -65,7 +65,7 @@ suite = {
             },
             {
                 "name": "graalpython",
-                "version": "e41cb90f2d2e24dce873e68312e1da2b199a9dad",
+                "version": "3571ef9868ff2af98d275e6dfbb04c8d2ef39d8c",
                 "dynamic": True,
                 "urls": [
                     {"url": "https://github.com/graalvm/graalpython.git", "kind": "git"},
